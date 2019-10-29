f61b372456ee770d30f9e2db4a82a4b748e92267
Ameni121 <57158756+Ameni121@users.noreply.github.com>
2019-10-30 00:07:30 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,43 @@
+<!Doctype html>
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<title>exercice1</title>
+
+</head>
+
+
+
+<body>
+<?php
+ include 'dbconnexion.php';
+ $reponse= $bdd->query('SELECT * FROM students ');
+ echo'<table border="1">';
+ while($donnees = $reponse->fetch()){
+	echo'<tr>';
+	echo'<td>'.$donnees['id'].'</td>';
+	 echo'<td>'.$donnees['firstname'].'</td>';
+	 echo'<td>'.$donnees['lastname'].'</td>';
+	 echo'<td>'.$donnees['email'].'</td>';
+	 echo'<td>'.$donnees['phone'].'</td>';
+	echo'</tr>';
+ }
+
+echo '</table>';
+?>
+
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+    <!-- Popper JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+	</body>
+</html>
---
+++ update.php
@@ -0,0 +1,11 @@
+<?php
+    try {  include 'dbconnexion.php';   
+    } catch(Exception $e) {    
+         die('Erreur : '.$e->getMessage());   
+        }
+        $firstname=$_POST['firstname'];
+        $lastname=$_POST['lastname'];
+        $email=$_POST['email'];
+        $phone=$_POST['phone']; 
+        $nb_modifs = $cnx->exec("UPDATE students SET firstname='$firstname',lastname='$lastname',email='$email',phone='$phone' "); 
+    echo $nb_modifs . '  !';  
