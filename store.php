2bc205c671fa2b9ef35cdb411c10f7845037d81f
Ameni121 <57158756+Ameni121@users.noreply.github.com>
2019-10-30 00:51:33 +0100

    Update store.php

--- store.php
+++ store.php
@@ -1,35 +1,43 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-
+    <title>Document</title>
 </head>
 <body>
+<table border="1">
+      <tr>
+          <th>ID</th>
+          <th>Firstname</th>
+          <th>Lastname</th>
+          <th>Email</th>
+          <th>Phone</th>
+          <th>OpÃ©rations</th>
+          <th><a href="edit.php">Editer</a></th>
+      </tr>
 <?php
-(!empty($_POST)){
-$firstname = $_POST['firstname'];
-$lastname = $_POST['lastname'];
-$email = $_POST['email'];
-$phone = $_POST['phone'];
+include 'dbconnexion.php' ;
 
-include "dbconnexion.php";
-$nb=$cnx->exec("INSERT INTO students(firstname,lastname,email,phone) VALUES('$firstname', '$lastname','$email','$phone'");
-echo $nb.'la personne a bien été ajoutée!' ;
-
+$rep= $bdd->query('SELECT * FROM students');
+ while($data=$rep->fetch()){
+    echo'<tr>';
+    echo'<td>'.$data['id'].'</td>';
+    echo'<td>'.$data['firstname'].'</td>';
+    echo'<td>'.$data['lastname'].'</td>';
+    echo'<td>'.$data['email'].'</td>';
+    echo'<td>'.$data['phone'].'</td>';
+    echo'</tr>';
 }
 ?>
-
-
-
-
-
-
-
-
+</table>
+<br>
+<button type="submit" class="btn btn-primary"><a href="create.php">Nouvel Ã©tudiant</a></button>
 <!-- jQuery library -->
-   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
 <!-- Popper JS -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
